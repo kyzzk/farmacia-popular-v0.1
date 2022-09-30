5527a4be54efa1016423e2dd53f22096d08ab772
kaze <guillherme-morais@live.com>
2022-09-30 00:23:05 -0300

    ADD Bootstrap / Registration and Login

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, my_logout
+from .views import home, my_logout, register_page
 
 
 urlpatterns = [
     path('', home, name="home"),
+    path('register/', register_page, name='register'),
     path('logout/', my_logout, name="logout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm
 
+
+def register_page(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.save()
+
+            user = authenticate(request, username=user.username, password=request.POST['password1'])
+
+            if user is not None:
+                login(request, user)
+
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'register.html', context)
 
 def home(request):
     return render(request, 'home.html')
